40627147ef3a4ccfd88012d1d3e6620b2bed2c69
Rashmin Dandekar <rdandekar@slb.com>
2019-09-20 15:26:58 +0800

    Update

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -1,2 +1,23 @@
 class PropsController < ApplicationController
+    def new
+        @gas = Gas.find(params[:gas_id])
+    end
+    def create 
+        @gas = Gas.find(params[:gas_id])
+        @prop = @gas.props.create(prop_params)
+        @prop.getzfactor
+        @prop.save
+        redirect_to gas_prop_path(@gas.id,@prop.id)
+    end
+
+    def show
+        @gas = Gas.find(params[:gas_id])
+        @prop = @gas.props.find(params[:id])
+    end
+
+    private
+        def prop_params
+            params.require(:prop).permit(:pressure,:temperature,:zfactor,:bg, :viscosity)
+        end
+
 end
--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,3 +1,7 @@
 class Prop < ApplicationRecord
   belongs_to :gas
+
+  def getzfactor
+    @zfactor=99.999
+  end
 end
