343bec56437152a21372dc3d51fb762c20d65b3b
Rashmin Dandekar <rdandekar@slb.com>
2019-09-23 15:54:40 +0800

    fixed show properties

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -4,15 +4,16 @@ class PropsController < ApplicationController
     end
     def create 
         @gas = Gas.find(params[:gas_id])
-        @prop = @gas.props.create(prop_params)
-        @prop.getzfactor
-        @prop.save
+        @prop = @gas.props.new(prop_params)
+        #@prop.get_zfactor
+        #@prop.save
         redirect_to gas_prop_path(@gas.id,@prop.id)
     end
 
     def show
         @gas = Gas.find(params[:gas_id])
         @prop = @gas.props.find(params[:id])
+        #@prop.get_zfactor
     end
 
     private
